2cb7ebc888e4605b318e8e47d49696dfefbddaad
luzi82 <luzi82@gmail.com>
2018-02-01 18:23:20 +0800

    sperate paint_text and add test func

--- python3/image_ocr.py
+++ python3/image_ocr.py
@@ -12,11 +12,8 @@ import itertools
 import codecs
 import re
 import datetime
-import cairocffi as cairo
 import editdistance
 import numpy as np
-from scipy import ndimage
-#import pylab
 from keras import backend as K
 from keras.layers.convolutional import Conv2D, MaxPooling2D
 from keras.layers import Input, Dense, Activation
@@ -30,6 +27,7 @@ from keras.preprocessing import image
 import keras.callbacks
 import json
 import random
+import paint_text as _paint_text
 
 
 OUTPUT_DIR = 'image_ocr'
@@ -40,67 +38,7 @@ alphabet = u'abcdefghijklmnopqrstuvwxyz '
 
 np.random.seed(55)
 
-
-# this creates larger "blotches" of noise which look
-# more realistic than just adding gaussian noise
-# assumes greyscale with pixels ranging from 0 to 1
-
-def speckle(img):
-    severity = np.random.uniform(0, 0.6)
-    blur = ndimage.gaussian_filter(np.random.randn(*img.shape) * severity, 1)
-    img_speck = (img + blur)
-    img_speck[img_speck > 1] = 1
-    img_speck[img_speck <= 0] = 0
-    return img_speck
-
-
-# paints the string in a random location the bounding box
-# also uses a random font, a slight random rotation,
-# and a random amount of speckle noise
-
-def paint_text(text, w, h, rotate=False, ud=False, multi_fonts=False):
-    surface = cairo.ImageSurface(cairo.FORMAT_RGB24, w, h)
-    with cairo.Context(surface) as context:
-        context.set_source_rgb(1, 1, 1)  # White
-        context.paint()
-        # this font list works in CentOS 7
-        if multi_fonts:
-            fonts = ['Century Schoolbook', 'Courier', 'STIX', 'URW Chancery L', 'FreeMono']
-            context.select_font_face(np.random.choice(fonts), cairo.FONT_SLANT_NORMAL,
-                                     np.random.choice([cairo.FONT_WEIGHT_BOLD, cairo.FONT_WEIGHT_NORMAL]))
-        else:
-            context.select_font_face('Courier', cairo.FONT_SLANT_NORMAL, cairo.FONT_WEIGHT_BOLD)
-        context.set_font_size(25)
-        box = context.text_extents(text)
-        border_w_h = (4, 4)
-        if box[2] > (w - 2 * border_w_h[1]) or box[3] > (h - 2 * border_w_h[0]):
-            raise IOError('Could not fit string into image. Max char count is too large for given image width.')
-
-        # teach the RNN translational invariance by
-        # fitting text box randomly on canvas, with some room to rotate
-        max_shift_x = w - box[2] - border_w_h[0]
-        max_shift_y = h - box[3] - border_w_h[1]
-        top_left_x = np.random.randint(0, int(max_shift_x))
-        if ud:
-            top_left_y = np.random.randint(0, int(max_shift_y))
-        else:
-            top_left_y = h // 2
-        context.move_to(top_left_x - int(box[0]), top_left_y - int(box[1]))
-        context.set_source_rgb(0, 0, 0)
-        context.show_text(text)
-
-    buf = surface.get_data()
-    a = np.frombuffer(buf, np.uint8)
-    a.shape = (h, w, 4)
-    a = a[:, :, 0]  # grab single channel
-    a = a.astype(np.float32) / 255
-    a = np.expand_dims(a, 0)
-    if rotate:
-        a = image.random_rotation(a, 3 * (w - top_left_x) / w + 1)
-    a = speckle(a)
-
-    return a
-
+paint_text = _paint_text.paint_text
 
 # Translation of characters to unique integer values
 def text_to_labels(text):
