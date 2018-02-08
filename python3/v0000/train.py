9df701bd97e8ef1f7b54489759d1495a785c5163
luzi82 <luzi82@gmail.com>
2018-02-08 12:44:19 +0800

    seperate model, make code running

--- python3/v0000/train.py
+++ python3/v0000/train.py
@@ -32,6 +32,7 @@ from . import paint_text as _paint_text
 import time
 import c
 import string
+from . import model as my_model
 
 OUTPUT_DIR = 'output'
 
@@ -167,17 +168,8 @@ def train(run_name, epochs, img_w):
     c.reset_dir(output_dir)
 
     # Network parameters
-    conv_filters = 16
-    kernel_size = (3, 3)
-    pool_size = 2
-    time_dense_size = 32
-    rnn_size = 512
-    minibatch_size = 32
-
-    if K.image_data_format() == 'channels_first':
-        input_shape = (1, img_w, img_h)
-    else:
-        input_shape = (img_w, img_h, 1)
+    pool_size = my_model.pool_size
+    minibatch_size = my_model.minibatch_size
 
     fdir = os.path.dirname(get_file('wordlists.tgz',
                                     origin='http://www.mythic-ai.com/datasets/wordlists.tgz', untar=True))
@@ -189,36 +181,8 @@ def train(run_name, epochs, img_w):
         downsample_factor=(pool_size ** 2),
         val_split=words_per_epoch - val_words
     )
-    act = 'relu'
-    input_data = Input(name='the_input', shape=input_shape, dtype='float32')
-    inner = Conv2D(conv_filters, kernel_size, padding='same',
-                   activation=act, kernel_initializer='he_normal',
-                   name='conv1')(input_data)
-    inner = MaxPooling2D(pool_size=(pool_size, pool_size), name='max1')(inner)
-    inner = Conv2D(conv_filters, kernel_size, padding='same',
-                   activation=act, kernel_initializer='he_normal',
-                   name='conv2')(inner)
-    inner = MaxPooling2D(pool_size=(pool_size, pool_size), name='max2')(inner)
 
-    conv_to_rnn_dims = (img_w // (pool_size ** 2), (img_h // (pool_size ** 2)) * conv_filters)
-    inner = Reshape(target_shape=conv_to_rnn_dims, name='reshape')(inner)
-
-    # cuts down input size going into RNN:
-    inner = Dense(time_dense_size, activation=act, name='dense1')(inner)
-
-    # Two layers of bidirectional GRUs
-    # GRU seems to work as well, if not better than LSTM:
-    gru_1 = GRU(rnn_size, return_sequences=True, kernel_initializer='he_normal', name='gru1')(inner)
-    gru_1b = GRU(rnn_size, return_sequences=True, go_backwards=True, kernel_initializer='he_normal', name='gru1_b')(inner)
-    gru1_merged = add([gru_1, gru_1b])
-    gru_2 = GRU(rnn_size, return_sequences=True, kernel_initializer='he_normal', name='gru2')(gru1_merged)
-    gru_2b = GRU(rnn_size, return_sequences=True, go_backwards=True, kernel_initializer='he_normal', name='gru2_b')(gru1_merged)
-
-    # transforms RNN output to character activations:
-    inner = Dense(img_gen.get_output_size(), kernel_initializer='he_normal',
-                  name='dense2')(concatenate([gru_2, gru_2b]))
-    y_pred = Activation('softmax', name='softmax')(inner)
-    Model(inputs=input_data, outputs=y_pred).summary()
+    input_data, y_pred = my_model.create_tensor_io(img_w, img_h, img_gen.get_output_size())
 
     labels = Input(name='the_labels', shape=[img_gen.absolute_max_string_len], dtype='float32')
     input_length = Input(name='input_length', shape=[1], dtype='int64')
